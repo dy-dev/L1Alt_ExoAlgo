c96d6b049cfcdb08a1684e9fb0da5c141f8481fd
arcreaneDev <dominique@arcreane.com>
2022-01-24 10:36:19 +0100

    Corrrection first part Exercice 1 on discount

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    float price = 10.f;
+        float finalprice = 0.5f * price;
+
+        finalprice *= 0.7f;
+
+        System.out.println("Final Price is : " + finalprice);
     }
 }
